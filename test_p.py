2dceaa035c9b825d1c45711a27eb026317df4a07
Vlad071109 <Vlad110907@yandex.ru>
2025-03-05 13:21:30 +0300

    Add files via upload

---
+++ test_p.py
@@ -0,0 +1,32 @@
+# import pytest
+
+from function import (
+    is_string,
+    reverse_string,
+    is_palindrome,
+    count_vowels,
+    remove_first_occurrence,
+)
+
+def test_is_string():
+    assert is_string("") == True
+    assert is_string("abc") == False
+    assert is_string("123") == False
+
+def test_reverse_string():
+    assert reverse_string("abc") == "cba"
+    assert reverse_string("Python") == "nohtyP"
+
+def test_is_palindrome():
+    assert is_palindrome("Race car") == True
+    assert is_palindrome("hello") == False
+    assert is_palindrome("Тропа налево повела на порт") == True
+
+def test_count_vowels():
+    assert count_vowels("hello") == 2
+    assert count_vowels("оловянный") == 4
+    assert count_vowels("стринг") == 2
+
+def test_remove_first_occurrence():
+    assert remove_first_occurrence("hello") == "helo"
+    assert remove_first_occurrence("aabbcc") == "abc"
